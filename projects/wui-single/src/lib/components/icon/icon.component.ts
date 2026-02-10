5e8edd2269783e10d896b19f8eb5a45420d2eb2e
Kusen Abdullah <kusena322@gmail.com>
2026-02-10 10:42:36 +0700

    update icon

--- projects/wui-single/src/lib/components/icon/icon.component.ts
+++ projects/wui-single/src/lib/components/icon/icon.component.ts
@@ -1,15 +1,36 @@
-import { Component, HostBinding, input as inputSignal } from '@angular/core';
+import { Component, input as inputSignal, computed } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'wui-icon',
   standalone: true,
-  template: ``,
+  template: `<span [innerHTML]="svgIcon()"></span>`,
+  styles: [`
+    :host {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      width: 24px;
+      height: 24px;
+    }
+    :host ::ng-deep svg {
+      width: 100%;
+      height: 100%;
+      fill: currentColor;
+    }
+  `]
 })
 export class IconComponent {
+  // Accept SVG path data from @mdi/js
   readonly icon = inputSignal<string>('');
 
-  @HostBinding('class')
-  get hostClass(): string {
-    return this.icon() ? `mdi mdi-${this.icon()}` : 'mdi';
-  }
+  readonly svgIcon = computed<SafeHtml>(() => {
+    const pathData = this.icon();
+    if (!pathData) return '';
+    
+    const svg = `<svg viewBox="0 0 24 24"><path d="${pathData}"/></svg>`;
+    return this.sanitizer.bypassSecurityTrustHtml(svg);
+  });
+
+  constructor(private sanitizer: DomSanitizer) {}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DrawerComponent, DrawerItemComponent, AppComponent as WuiAppComponent } from '@wajek/wui';
+import { mdiHome, mdiTextLong, mdiLoginVariant, mdiTable } from '@mdi/js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WuiAppComponent, DrawerComponent,DrawerItemComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, WuiAppComponent, DrawerComponent, DrawerItemComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  // Expose icons to template - only these 4 icons will be bundled!
+  readonly icons = {
+    home: mdiHome,
+    textLong: mdiTextLong,
+    loginVariant: mdiLoginVariant,
+    table: mdiTable
+  };
 }
